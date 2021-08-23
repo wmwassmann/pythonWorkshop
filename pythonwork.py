abf332bd466af8a627c8c86b959b8e065af2c116
William Wassmann <wmwassmann@hotmail.com>
2021-08-22 22:23:58 -0700

    initial commit for my python journey

---
+++ pythonwork.py
@@ -0,0 +1,87 @@
+print('hello world')
+
+# Booleans, Numbers, Int, Strings, Tuples, Lists (Array), Dictionary (Object),
+#  {} Bye {} No {brackets}
+
+# if 1==1:
+#     print('hi')
+
+
+# lead = 'My name is '
+# name = 'Bob'
+
+# combined = lead + name
+
+# print(combined)
+
+
+
+
+# print(colors)
+
+
+# numberOne = 11
+# numberTwo = 2
+
+# if numberOne==numberOne:
+#     print('wtf no they aren\'t')
+# elif numberOne==numberTwo:
+#     print('Goooooood')
+
+
+# if numberOne < numberTwo:
+#     print('numberOne is lesser')
+# else:
+#     print('numberOne is greater')
+
+
+# if numberOne > numberTwo and numberOne < 25: 
+#     print('correct!')
+# else:
+#     print('Incorrect')
+
+
+# # for color in colors:
+# #     print(color)
+
+# for number in range(1, 6):
+#     print(number)
+
+# colors = ['red', 'blue', 'green', 'purple?', 'yellow', 'orange', 'pink!', 'black', 'other blue', 'sad blue']
+
+# i=0
+
+# while colors[i] !='black':
+#     print(colors[i])
+#     i+=1
+
+
+
+word = 'buffalo'
+
+
+# for letter in word:
+#     print(letter)
+#     if letter=='f':
+#         pass
+
+
+def addFunction(numOne, numTwo): 
+    sum = numOne + numTwo
+    return sum
+
+add = addFunction(10, 191)
+
+print(add)
+
+
+# ALGO ONE print 1-255
+
+
+for number in range(1, 256):
+    print(number)
+
+
+for number in range(1, 256):
+    if number % 2!=0:
+        print(number)
